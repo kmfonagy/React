e974ff16a0521ad03f13d6c9073c418f6483ac40
Kristofer Fonagy <kristofer@fonagy.com>
2021-02-13 20:49:02 -0500

    Update state_demo

--- 06_Intro_State/state_demo/src/App.js
+++ 06_Intro_State/state_demo/src/App.js
@@ -1,13 +1,24 @@
-import React, { Component } from 'react';
-import Game from './Game';
-import './App.css';
+import React, { Component } from "react";
+import Game from "./Game";
+import Demo from "./Demo";
+import Button from "./Button";
+import BrokenClick from "./BrokenClick";
+import Rando from "./Rando";
+import Clicker from "./Clicker";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Game />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div className='App'>
+        <Game />
+        <Demo />
+        <Button />
+        <Rando maxNum={10} />
+        <Clicker />
+      </div>
+    );
+  }
 }
 
 export default App;
